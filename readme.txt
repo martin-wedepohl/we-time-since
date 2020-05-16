33d7d6beb74e9a1a146367ae9910b905b395ce25
Martin Wedepohl <martin.wedepohl@shaw.ca>
2020-05-15 23:35:14 -0700

    1.0.0 Initial release

---
+++ readme.txt
@@ -0,0 +1,94 @@
+=== Wedepohl Engineering Time Since ===
+Contributors: mwedepohl
+Donate link: https://paypal.me/martinwedepohl
+Tags: time, span, years, days
+Requires at least: 4.9
+Tested up to: 5.4.1
+Stable tag: 1.0.0
+Requires PHP: 5.6
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+
+Shortcode to display the number of years or days since a certain date.
+
+== Description ==
+
+Shortcode to return the number of years or days since a certain date.
+
+Usage: `[we_time_since y=Y m=M d=D type="T"]`
+
+Where:
+* Y = the year **required**
+* M = the month (1 - 12) - **optional** Default = **1**
+* D = the day (1-31) - **optional** Default = **1**
+* T = the type ("year", "day") - **optional** Default = **"year"**
+
+Returns: 
+The number of years or days since the supplied date or an error message if the date is invalid / no year supplied
+
+= Examples =
+
+I have worked at Wedepohl Engineering for `[we_time_since y=1998 m=1 d=1]` years 
+
+I have been sailing for `[we_time_since y=2008 m=4]` years
+
+I have been on this journey for `[we_time_since y=2020 m=5 d=5 type="day"]` days
+
+= Actions =
+
+`before_we_time_since` - Called upon entry to the shortcode. Passed the array of arguments that are passed to the shortcode.
+`after_we_time_since` - Called just before exit of the function. Passed the processed array of arguments from the shortcode.
+
+= Filters =
+
+`we_time_since` Called at exit of the function. Passed a string which is the results of the shortcode.
+
+= Active Contributors =
+<li>[Martin Wedepohl](https://profiles.wordpress.org/mwedepohl/) (Development)</li>
+
+== Installation ==
+
+1. Upload `we-disable-fs` folder to the `/wp-content/plugins/` directory
+2. Navigate to the plugins admin page
+3. Click the **Activate** link
+4. That's all (no further configuration or settings required)
+
+or 
+
+1. Navigate to the plugins admin page
+2. Click the **Add New** button
+3. Search for `Wedepohl Engineering Time Since`
+4. Click the **Install Now** button
+5. Click the **Active** button
+6. That's all (no further configuration or settings required)
+
+= Removal Instructions =
+1. Navigate to the plugins admin page
+2. Click the **Deactivate** link
+3. Click the **Delete** link
+4. That's all (plugin doesn't use the database so there is nothing to cleanup)
+
+== Frequently Asked Questions ==
+
+= Settings =
+There are none, just activate and use the shortcode
+
+= Database =
+This plugin doesn't use the database at all
+
+= Does this work with WordPress blocks =
+Yes, just enter the shortcode in the text of a paragraph or heading
+
+= Does this work with ClassicPress =
+Yes, there is nothing WordPress specific
+
+= Issues/Bugs/Features =
+1. Go to [Plugin GitHub Page](https://github.com/martin-wedepohl/we-time-since)
+2. Create an Issue
+3. I will endevor to resolve issues as soon as time permits
+
+== Changelog ==
+
+= 1.0.0 =
+May 15, 2020
+* Initial release
---
+++ we-time-since.php
@@ -0,0 +1,121 @@
+<?php    // phpcs:ignore
+/**
+ * Display the time (years/days) since a certain date
+ *
+ * @package           WETimeSince
+ * @author            Martin Wedepohl
+ * @copyright         2020 Wedepohl Engineering
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WE Time Since
+ * Plugin URI:        https://github.com/martin-wedepohl/we-time-since
+ * Description:       Display the time (years/days) since a certain date.
+ * Version:           1.0.0
+ * Requires at least: 4.9
+ * Requires PHP:      5.6
+ * Author:            Martin Wedepohl
+ * Author URI:        https://wedepohlengineering.com/
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       we-time-since
+ *
+ * The plugin we-time-since is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * we-time-since is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with we_time-since. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+if ( ! class_exists( 'WETimeSince' ) ) {
+
+	/**
+	 * Class WETimeSince
+	 *
+	 * Provides all the functionality for a shortcode that will display the number
+	 * of years or days since a certain date.
+	 *
+	 * @package WETimeSince
+	 */
+	class WETimeSince {
+
+		/**
+		 * Class constructor adds the shortcode.
+		 *
+		 * @since 1.0.0
+		 */
+		public function __construct() {
+
+			add_shortcode( 'we_time_since', array( $this, 'time_since' ) );
+
+		}
+
+		/**
+		 * Return the time since a date.
+		 *
+		 * Format [we_time_since y=Y m=M d=D type='T']
+		 * Where  Y is the year (required)
+		 *        M is the month 1-12 (optional) - default 1
+		 *        D is the day   1-31 (optional) - default 1
+		 *        T is the type  year or day (optional) - default year
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array $atts Attributes passed in from the shortcode.
+		 *
+		 * @return string Text string for time since the date.
+		 */
+		public function time_since( $atts ): string {
+
+			do_action( 'before_we_time_since', $atts );
+
+			// Process the attributes.
+			$atts = shortcode_atts(
+				array(
+					'y'    => 0,
+					'm'    => 1,
+					'd'    => 1,
+					'type' => 'year',
+				),
+				$atts,
+				'we_time_since'
+			);
+
+			if ( true === checkdate( $atts['m'], $atts['d'], $atts['y'] ) ) {
+				$now      = \current_time( 'Y-m-d' );
+				$now_date = new \DateTime( $now );
+				$date     = new \DateTime( "{$atts['y']}-{$atts['m']}-{$atts['d']}" );
+				$diff     = $date->diff( $now_date );
+				if ( 'year' === $atts['type'] ) {
+					$html = $diff->format( '%y' );
+				} else {
+					$html = $diff->format( '%a' );
+				}
+			} else {
+				$html = __( 'ERROR the date supplied is invalid', 'we-time-since' );
+			}
+
+			do_action( 'after_we_time_since', $atts );
+
+			$html = apply_filters( 'we_time_since', $html );
+
+			return $html;
+
+		}
+
+	}
+
+	new WETimeSince();
+
+}
